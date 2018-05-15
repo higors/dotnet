115e00f1c3d7ad8eb84b82d2f0cca1ebb298c94c
Lucas Nascimento Aguiar <lucasaguiar@cit>
2018-05-15 10:35:30 -0300

    alterar solution para a raiz do repositorio

---
+++ Treinamento.DataAccess/SqlOrder.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Treinamento.Entities;
+using Treinamento.Interfaces;
+
+namespace Treinamento.DataAccess
+{
+    public class SqlOrder : IOrder
+    {
+        public String connectionString
+        {
+            get
+            {
+                return System.Configuration.ConfigurationManager.ConnectionStrings["MY_DB"].ConnectionString;
+            }
+        }
+        void IOrder.DeleteOrder(uint Id)
+        {
+            var stringBuilder = new StringBuilder();
+
+            stringBuilder.AppendLine("DELETE ORDERS WHERE ORD_ID = @ordId");
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            using (SqlCommand command = new SqlCommand(stringBuilder.ToString(), connection))
+            {
+                var parameter = new SqlParameter()
+                {
+                    ParameterName = "@ordId",
+                    SqlDbType = System.Data.SqlDbType.Int,
+                    Value = Id
+                };
+
+                command.Parameters.Add(parameter);
+            }
+        }
+
+        IList<Order> IOrder.ListOrder()
+        {
+            throw new NotImplementedException();
+        }
+
+        void IOrder.NewOrder(Order order)
+        {
+            var stringBuilder = new StringBuilder();
+
+            stringBuilder.AppendLine("INSERT INTO ORDERS (ORD_DESCRIPTION, ORD_NUMBER, ORD_DATE)");
+            stringBuilder.AppendLine("VALUES (@ord_description, @ord_number, )");
+        }
+
+        void IOrder.UpdateOrder(Order order)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
