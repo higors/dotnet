f7b4bf0a666ba0c5ca82949773164d4831a592d0
Lucas Nascimento Aguiar <lucasaguiar@cit>
2018-07-02 12:03:36 -0300

    implementação dos metodos de sql

--- Treinamento.DataAccess/SqlOrder.cs
+++ Treinamento.DataAccess/SqlOrder.cs
@@ -35,8 +35,10 @@ namespace Treinamento.DataAccess
                         SqlDbType = System.Data.SqlDbType.Int,
                         Value = Id
                     };
-                    
+
                     command.Parameters.Add(parameter);
+
+                    connection.Open();
 
                     command.ExecuteNonQuery();
                 }
@@ -45,15 +47,61 @@ namespace Treinamento.DataAccess
 
         IList<Order> IOrder.ListOrder()
         {
-            throw new NotImplementedException();
+            var stringBuilder = new StringBuilder();
+
+            stringBuilder.AppendLine("SELECT ORDERS.ORD_ID, ORDERS.ORD_NUMBER, ORDERS.ORD_DESCRIPTION, ORDERS.ORD_DATE, ORDER_TYPES.ORT_DESCRIPTION");
+            stringBuilder.AppendLine("FROM ORDERS INNER JOIN ORDER_TYPES ON ORDERS.ORD_ORT_ID = ORDER_TYPES.ORT_ID");
+
+            using (var connection = new SqlConnection(GetConnectionString))
+            using (var command = new SqlCommand(stringBuilder.ToString(), connection))
+            {
+                connection.Open();
+
+                var lista = command.ExecuteReader().OfType<Order>().ToList;
+            }
+
         }
 
         void IOrder.NewOrder(Order order)
         {
             var stringBuilder = new StringBuilder();
 
-            stringBuilder.AppendLine("INSERT INTO ORDERS (ORD_DESCRIPTION, ORD_NUMBER, ORD_DATE)");
-            stringBuilder.AppendLine("VALUES (@ord_description, @ord_number, )");
+            stringBuilder.AppendLine("INSERT INTO ORDERS (ORD_DESCRIPTION, ORD_NUMBER, ORD_DATE, ORD_ORT_ID)");
+            stringBuilder.AppendLine("VALUES (@ord_description, @ord_number, @ord_date, @ord_ort_id)");
+
+            using (var connection = new SqlConnection(GetConnectionString))
+            using (var command = new SqlCommand(stringBuilder.ToString(), connection))
+            {
+                IList<SqlParameter> listOfParameters = new List<SqlParameter>
+                {
+                    new SqlParameter()
+                    {
+                        ParameterName = "@ord_description",
+                        SqlDbType = System.Data.SqlDbType.VarChar,
+                        Value = order.Description
+                    },
+
+                    new SqlParameter()
+                    {
+                        ParameterName = "@ord_number",
+                        SqlDbType = System.Data.SqlDbType.VarChar,
+                        Value = order.OrderNumber
+                    },
+
+                    new SqlParameter()
+                    {
+                        ParameterName = "@ord_date",
+                        SqlDbType = System.Data.SqlDbType.DateTime,
+                        Value = order.OrderDate
+                    }
+                };
+
+                command.Parameters.AddRange(listOfParameters.ToArray());
+
+                connection.Open();
+
+                command.ExecuteNonQuery();
+            }
         }
 
         void IOrder.UpdateOrder(Order order)
