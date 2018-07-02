e1231057af5f26f2839d033e23173fdd8990c9cd
Lucas Nascimento Aguiar <lucasaguiar@cit>
2018-07-02 12:05:03 -0300

    implementação dos metodos de sql

--- Treinamento.DataAccess/SqlOrder.cs
+++ Treinamento.DataAccess/SqlOrder.cs
@@ -57,7 +57,7 @@ namespace Treinamento.DataAccess
             {
                 connection.Open();
 
-                var lista = command.ExecuteReader().OfType<Order>().ToList();
+                return command.ExecuteReader().OfType<Order>().ToList();
             }
 
         }
